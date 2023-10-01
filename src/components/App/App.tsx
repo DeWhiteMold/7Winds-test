2c7956d14e90dd6b32932e8e138e37fe403b684a
Nikolai Smirnov <dewhitemold@yandex.ru>
2023-10-01 15:13:20 +0200

    added fonts, connected header and main, added style and theme consts

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Header from './Header/Header'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import './App.scss'
+import { ThemeProvider } from '@emotion/react'
+import { theme } from '../../consts/themeConsts'
+import Main from './Main/Main'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <ThemeProvider theme={theme} >
+      <div className="App">
+        <Header />
+        <Main />
+      </div>
+    </ThemeProvider>
   )
 }
 
---
+++ src/consts/themeConsts.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FFFFFF'
+    },
+    secondary: {
+      main: '#A1A1AA'
+    }
+  }
+})
+
+export { theme }
